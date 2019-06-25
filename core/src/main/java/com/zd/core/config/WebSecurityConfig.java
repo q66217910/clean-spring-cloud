dee7864039fab8327146f4a96b1d2aa59268efa0
dezhang <dezhang@homeinns.com>
2019-06-25 15:17:47 +0800

    oauth2

---
+++ core/src/main/java/com/zd/core/config/WebSecurityConfig.java
@@ -0,0 +1,12 @@
+package com.zd.core.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+}
