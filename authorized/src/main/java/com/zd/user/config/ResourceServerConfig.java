64a505f3f860512e39972d3fe44f344a4e99df61
dezhang <dezhang@homeinns.com>
2019-07-06 00:20:55 +0800

    jwt

--- authorized/src/main/java/com/zd/user/config/ResourceServerConfig.java
+++ authorized/src/main/java/com/zd/user/config/ResourceServerConfig.java
@@ -1,11 +1,17 @@
 package com.zd.user.config;
 
 import com.zd.user.config.properties.Oauth2Property;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,6 +24,32 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     public ResourceServerConfig(Oauth2Property oauth2Property) {
         this.oauth2Property = oauth2Property;
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        super.configure(resources);
+        resources.tokenServices(tokenServices());
+    }
+
+    @Bean
+    public JwtTokenStore jwtTokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("jwt");
+        return converter;
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(jwtTokenStore());
+        return defaultTokenServices;
     }
 
     @Override
