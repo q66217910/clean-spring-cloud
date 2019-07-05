e143ef2f47b62ead96136836ca1ab2e1bb85e27f
dezhang <dezhang@homeinns.com>
2019-07-05 11:06:16 +0800

    geteway

---
+++ geteway/src/main/java/com/zd/geteway/GetewayApplication.java
@@ -0,0 +1,14 @@
+package com.zd.geteway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class GetewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetewayApplication.class, args);
+    }
+
+}
