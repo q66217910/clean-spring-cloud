9f884150f6e8b826743396d8b7a90196eebeb32b
dezhang <dezhang@homeinns.com>
2019-07-14 20:37:20 +0800

    zipkin-server springboot2.0无法启动

--- zipkin/src/main/java/com/zd/zipkin/ZipkinApplication.java
+++ zipkin/src/main/java/com/zd/zipkin/ZipkinApplication.java
@@ -2,11 +2,12 @@ package com.zd.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
-import zipkin2.server.internal.EnableZipkinServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin.server.EnableZipkinServer;
 
+@EnableDiscoveryClient
 @EnableZipkinServer
-@SpringCloudApplication
+@SpringBootApplication
 public class ZipkinApplication {
 
     public static void main(String[] args) {
