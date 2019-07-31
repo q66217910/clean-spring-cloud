9b9ef717c61042980468ed98732375dbeea0a532
dezhang <dezhang@homeinns.com>
2019-07-31 14:40:04 +0800

    config

---
+++ config/src/main/java/com/zd/config/ConfigApplication.java
@@ -0,0 +1,15 @@
+package com.zd.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringCloudApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
