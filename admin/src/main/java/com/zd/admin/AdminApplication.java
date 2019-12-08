65b3371024cbc0edcfc9c99b60e98f20e894855d
koala <670677781@qq.com>
2019-12-08 23:57:33 +0800

    admin

--- admin/src/main/java/com/zd/admin/AdminApplication.java
+++ admin/src/main/java/com/zd/admin/AdminApplication.java
@@ -2,10 +2,12 @@ package com.zd.admin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
 @EnableAdminServer
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 public class AdminApplication {
 
     public static void main(String[] args) {
