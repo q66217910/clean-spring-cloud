11fb1c9706bd1d0e597802cb09ab9da7edba8bc9
koala <670677781@qq.com>
2020-02-17 11:57:30 +0800

    ADMIN

--- admin/src/main/java/com/zd/admin/AdminApplication.java
+++ admin/src/main/java/com/zd/admin/AdminApplication.java
@@ -1,14 +1,10 @@
 package com.zd.admin;
 
-import com.zd.core.interfaces.EnableCommonScan;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.context.annotation.Configuration;
 
 @EnableAdminServer
-@EnableCommonScan
 @SpringCloudApplication
 public class AdminApplication {
 
