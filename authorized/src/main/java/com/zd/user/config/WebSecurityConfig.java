1b4af787050c252af7db1bee841e70a480288349
koala <670677781@qq.com>
2020-02-24 00:34:49 +0800

    ENTITY

--- authorized/src/main/java/com/zd/user/config/WebSecurityConfig.java
+++ authorized/src/main/java/com/zd/user/config/WebSecurityConfig.java
@@ -55,7 +55,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/favicon.ico","/webjars/**","/v2/api-docs","/swagger-resources/**","/swagger-ui.html").permitAll()
                 .and()
                 .authorizeRequests()
-                .anyRequest().permitAll()
+                .anyRequest().authenticated()
                 .and().httpBasic().and().csrf().disable();
     }
 }
