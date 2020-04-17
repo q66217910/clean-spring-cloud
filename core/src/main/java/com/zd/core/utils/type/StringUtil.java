8ea40534e321c01774832ff27e8be14d435018ff
dezhang <dezhang@homeinns.com>
2020-04-17 17:22:47 +0800

    leetcode

--- core/src/main/java/com/zd/core/utils/type/StringUtil.java
+++ core/src/main/java/com/zd/core/utils/type/StringUtil.java
@@ -654,6 +654,24 @@ public class StringUtil {
         return dp[0][0][n];
     }
 
+    /**
+     * 数字转罗马数字
+     */
+    public String intToRoman(int num) {
+        int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        while (index < 13) {
+            while (num >= nums[index]) {
+                sb.append(romans[index]);
+                num -= nums[index];
+            }
+            index++;
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         System.out.println(new StringUtil().myAtoi("+-1"));
     }
