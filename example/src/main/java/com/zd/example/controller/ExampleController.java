7990d72cd23158c62e8b031f8cf04aeb402b7ecf
dezhang <dezhang@homeinns.com>
2020-08-27 17:43:35 +0800

    ouath2

--- example/src/main/java/com/zd/example/controller/ExampleController.java
+++ example/src/main/java/com/zd/example/controller/ExampleController.java
@@ -4,12 +4,16 @@ import com.zd.core.bean.ResultBean;
 import com.zd.entity.user.AuthUser;
 import com.zd.feign.api.ExampleApi;
 import com.zd.feign.api.user.UserApi;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.cloud.sleuth.instrument.async.TraceableExecutorService;
+import org.springframework.integration.annotation.IdempotentReceiver;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
 
 @RestController
 @RequestMapping("/example")
