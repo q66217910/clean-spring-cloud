d76833413bcbb58dfffe3e961993946ddfd1ca3e
dezhang <dezhang@homeinns.com>
2020-08-28 17:10:59 +0800

    ouath2

--- eureka/src/main/java/com/zd/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/zd/eureka/EurekaApplication.java
@@ -3,12 +3,15 @@ package com.zd.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
     }
